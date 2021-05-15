e931a602f74e331fdec626d696ad2ba2421b3b3f
Nishi Gautam <nishigautam.nishu2@gmail.com>
2021-05-16 02:44:17 +0530

    Ability to create AddressBookService Database

---
+++ AddressBookServiceDB.sql
@@ -0,0 +1,14 @@
+#usecase1
+
+mysql> CREATE DATABASE AddressBook_Service;
+Query OK, 1 row affected (0.02 sec)
+
+mysql> USE AddressBook_Service;
+Database changed
+mysql> SELECT DATABASE();
++---------------------+
+| DATABASE()          |
++---------------------+
+| addressbook_service |
++---------------------+
+1 row in set (0.00 sec)
